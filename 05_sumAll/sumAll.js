d82bd47ed247b58aef6f36c82bfef81ff3660c20
Thatcher <thatcher@Thatchers-iMac.local>
2023-07-25 17:16:05 -0400

    Finished 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function(firstNum, secondNum) {
+        if (firstNum < 0 || secondNum < 0) {
+                return "ERROR";
+        }
+        if (!Number.isInteger(firstNum) || !Number.isInteger(secondNum)) {
+                return "ERROR";
+        }
+
+        if (firstNum > secondNum) {
+                const temp = firstNum;
+                firstNum = secondNum;
+                secondNum = temp;
+        }
+
+        let total = 0;
+        for (let i = firstNum; i <= secondNum; i++) {
+                total += i;
+        }
+        return total;
 
 };
 
