0fff1187a709691cc526424da6e31b8a5524f281
Th5tch3r <amber.phan27@gmail.com>
2023-08-11 19:31:52 -0400

    Finished 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(fib) {
+    if (fib < 0) return "OOPS";
+    if (fib === 0) return 0;
+
+    let firstFib = 1;
+    let secondFib = 0;
+
+    for (let i = 2; i <= fib; i++) {
+        let current = firstFib + secondFib;
+        secondFib = firstFib;
+        firstFib = current;
+    }
+
+    return firstFib;
 
 };
 
