390ab753233b60f82e460d84a37c6aa58673c527
Th5tch3r <amber.phan27@gmail.com>
2023-08-11 19:25:57 -0400

    Finished 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const adjustedString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+    return adjustedString.split("").reverse().join("") === adjustedString;
 };
 
 // Do not edit below this line
